2e615b515b36fdaaf521703710cf88699c2f4829
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-03-09 18:16:34 -0300

    fix:funcionando a funcao de reverter uma string

--- src/funcoes/funcoes_menu.rs
+++ src/funcoes/funcoes_menu.rs
@@ -2,13 +2,13 @@
 include!("dobro.rs");
 include!("introducao_for.rs");
 include!("verifica_caracteres_unicos.rs");
-
+include!("reverse_string.rs");
 fn funcoes_menu(){
     println!("Escolha uma opção:");
     println!("1. Dobro");
     println!("2. FOR");
     println!("3. Verifica Caracteres");
-
+    println!("4. Reverte uma String");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Falha ao ler a entrada");
     let choice: u32 = input.trim().parse().expect("Entrada inválida");
@@ -17,6 +17,7 @@ fn funcoes_menu(){
         1 => teste_dobro(),
         2 => introducao_for(),
         3 => verifica_caracteres(),
+        4 => reverve(),
         _ => println!("Opção inválida"),
     }
 
--- src/funcoes/reverse_string.rs
+++ src/funcoes/reverse_string.rs
@@ -1,7 +1,11 @@
-fn reverve_string(input: &str) -> String {
-let reserved: String = input.chars().rev().collect(); 
+fn reversed_string(input: &str) -> String {
+let reversed: String = input.chars().rev().collect(); 
+reversed
 }
 
 fn reverve(){
-
+let input_string = String::from("hello");
+let reversed_string1 = reversed_string(&input_string);
+println!("Original string {}", input_string);
+println!("Reverse string {}", reversed_string1);
 }
